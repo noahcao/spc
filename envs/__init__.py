d64ae048c49b7c4331416ffd6ec4daabdbb1abc8
noahcao <jinkuncao@gmail.com>
2019-08-22 13:14:04 -0700

    package the env creation for carla in the same handler as GTAV and Torcs

--- envs/__init__.py
+++ envs/__init__.py
@@ -16,24 +16,55 @@ class make_env(object):
                           ID=self.args.id)
             return TorcsWrapper(self.env)
 
-        elif 'carla' in self.args.env:
+        elif 'carla8' in self.args.env:
+            # run spc on carla0.9 simulator, currently only 0.9.4 is supported
             from carla.client import make_carla_client
             from .CARLA.carla_env import CarlaEnv
             with make_carla_client('localhost', self.args.port) as client:
                 return CarlaEnv(client)
+
+        elif 'carla9' in self.args.env:
+            # run spc on carla0.9 simulator, currently only 0.9.4 is supported
+            import glob
+            import os
+            import sys
+            from .CARLA.carla_env.world import World
+            try:
+                sys.path.append(glob.glob('**/*%d.%d-%s.egg' % (
+                    sys.version_info.major,
+                    sys.version_info.minor,
+                    'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+            except IndexError:
+                pass
+            import carla
+            client = carla.Client('localhost', self.args.port)
+            client.set_timeout(20.0)
+            carla_world = client.get_world()
+            settings = carla_world.get_settings()
+            settings.synchronous_mode = True
+            client.get_world().apply_settings(settings)
+            env = World(self.args, carla_world)
+            return env
+
 
         elif 'gta' in self.args.env:
             from .GTAV.gta_env import GtaEnv
             from .GTAV.gta_wrapper import GTAWrapper
             return GTAWrapper(GtaEnv(autodrive=None))
 
+        else:
+            assert(0)
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if 'torcs' in self.args.env:
             self.env.close()
 
-        elif 'carla' in self.args.env:
+        elif 'carla8' in self.args.env:
             pass  # self.client.__exit__(exc_type, exc_val, exc_tb)
+
+        elif 'carla9' in self.args.env:
+            pass
 
         elif 'gta' in self.args.env:
             pass
--- main.py
+++ main.py
@@ -19,42 +19,8 @@ if __name__ == '__main__':
     np.random.seed(args.seed)
     random.seed(args.seed)
 
-    if 'carla' in args.env:
-        if args.env == "carla8":
-            # run spc on carlav0.8 simulator, 0.8.4 is recommended
-            from carla.client import make_carla_client
-            from envs.CARLA.carla_env import CarlaEnv
-            clint = make_carla_client('localhost', args.port)
-            env = CarlaEnv(client)
-        elif args.env == "carla9":
-            # run spc on carla0.9 simulator, currently only 0.9.4 is supported
-            import glob
-            import os
-            import sys
-            from envs.CARLA.carla_env.world import World
-            try:
-                sys.path.append(glob.glob('**/*%d.%d-%s.egg' % (
-                    sys.version_info.major,
-                    sys.version_info.minor,
-                    'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-            except IndexError:
-                pass 
-            import carla
-            client = carla.Client('localhost', args.port)
-            client.set_timeout(20.0)
-            carla_world = client.get_world()
-            settings = carla_world.get_settings()
-            settings.synchronous_mode = True
-            client.get_world().apply_settings(settings)
-            env = World(args, carla_world)
-
+    with make_env(args) as env:
         if args.eval:
             evaluate_policy(args, env)
         else:
             train_policy(args, env, max_steps=args.max_steps)
-    else:
-        with make_env(args) as env:
-            if args.eval:
-                evaluate_policy(args, env)
-            else:
-                train_policy(args, env, max_steps=args.max_steps)
